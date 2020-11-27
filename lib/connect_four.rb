90ca5a1cc9f42be655547fb2ddff27a690eb70b2
enzoperce35 <enzoperce35@gmail.com>
2020-11-28 06:47:26 +0800

    modified

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,23 +1,14 @@
 require_relative 'grid.rb'
 
 class Game < Grid
-  #attr_accessor :player1, :player2, :game_board, :turn_count, :game_won, :vertical, :horizontal, :diagonal
-  attr_accessor :grid, :board, :new_input, :turn_count, :game_won, :player, :disc
+  attr_accessor :grid, :board, :new_input, :turn_count, :game_won
 
   def initialize
-    #@game_board = BOARD
-    #@turn_count = 0
-    #@vertical = [[],[],[],[],[],[],[]]
-    #@horizontal = [[],[],[],[],[],[]]
-    #@diagonal = []
-
     @grid = Grid.new
     @board = draw_board
     @turn_count = 0
     @game_won = false
     @new_input = {}
-
-
   end
 
   def draw_board
@@ -29,12 +20,12 @@ class Game < Grid
     rows.reverse + "\n  1  2  3  4  5  6  7"
   end
 
-  def start
+  def start  
     while !game_won
       puts "#{player.keys[0]}'s turn, choose slot"
       new_input.store('input', gets.chomp!.to_i)
 
-      valid_input? ? drop : redo
+      valid_input? ? update_grids : redo
       status
       puts draw_board
       puts "Game Over: #{player.keys[0]} wins" if game_won
@@ -56,77 +47,20 @@ class Game < Grid
     input = new_input.values[0]
     avail_slot = grid.vertical[input-1].count("\u26AA ")
 
-    new_input.store('row_num', 6 - avail_slot)
-    new_input.store('col_num', input-1)
-    new_input.store('rev_ind', (input-7).abs)  #reversed index for '#draw_board' return
+    new_input[:row_num] = 6 - avail_slot
+    new_input[:col_num] = input-1
+    new_input[:rev_ind] = (input-7).abs  #reversed index for '#draw_board' return
+    new_input[:unicode] = player.values[0]
 
     input.between?(1,7) && avail_slot > 0
   end
 
-  def drop
-    #{"input"=>3, "row_num"=>0, "col_num"=>2, "rev_ind"=>4}
-
-    row_index = new_input.values[1]
-    column_index = new_input.values[2]
-    reversed_index = new_input.values[3]
-
-    horizontal_grid(reversed_index)
-    vertical_grid(column_index)
-    diagonal_grid([column_index, row_index])
+  def update_grids
+    grid.update_horizontal(new_input[:rev_ind], new_input[:unicode])
+    grid.update_vertical(new_input[:col_num], new_input[:unicode])
+    grid.update_diagonal([new_input[:col_num], new_input[:row_num]], new_input[:unicode])
   end
-
-  def horizontal_grid(index)
-    slot_found = false
-    grid.horizontal.each do |line|
-      if line[index] == "\u26AA " && !slot_found
-        line[index] = player.values[0]
-        slot_found
-        break
-      end
-    end
-  end
-
-  def vertical_grid(index)
-    slot_found = false
-    grid.vertical[index].each_with_index do |slot,ind|
-      if slot == "\u26AA " && !slot_found
-        grid.vertical[index][ind] = player.values[0]
-        slot_found
-        break
-      end
-    end
-  end
-
-  def diagonal_grid(input)
-    grid.diagonal = [[],[]]
-    check_sequences(input, 'forward')
-    check_sequences(input, 'backward')
-  end
-
-  def diagonal_points(input, dir)
-    return nil if input.nil?
-    if dir == 'forward'
-      [[input[0]+1,input[1]+1],[input[0]-1,input[1]-1]]
-    elsif dir == 'backward'
-      [[input[0]-1,input[1]+1], [input[0]+1,input[1]-1]]
-    end
-  end
-
-  def check_sequences(input, dir, queue = [], checked = [], dir2 = diagonal_points(input,dir))
-    return nil if input.nil?
-
-    2.times do |x|
-      slot_attr = dir2.shift
-      if slot_attr.all? { |attr| attr.between?(0,6) }
-        queue << slot_attr if grid.vertical[slot_attr[0]][slot_attr[1]] == player.values[0]
-      end
-    end
-
-    grid.diagonal[dir == 'forward' ? 0 : 1] << player.values[0]
-    checked << input
-    check_sequences((queue -= checked).shift, dir, queue, checked)
-  end
-
+  #ruby ./lib/connect_four.rb
   def status
     connect_four = player.values[0] * 4
     grid.horizontal.each { |line| @game_won = true if line.join.include? connect_four}
@@ -135,3 +69,6 @@ class Game < Grid
     game_won
   end
 end
+
+x = Game.new
+x.start
--- lib/grid.rb
+++ lib/grid.rb
@@ -22,7 +22,56 @@ class Grid
     end
     columns
   end
-end
 
-#x = Grid.new
-#puts x.display
+  def update_horizontal(index, unicode)
+    slot_found = false
+    horizontal.each do |line|
+      if line[index] == "\u26AA " && !slot_found
+        line[index] = unicode
+        slot_found
+        break
+      end
+    end
+  end
+
+  def update_vertical(index, unicode)
+    slot_found = false
+    vertical[index].each_with_index do |slot,ind|
+      if slot == "\u26AA " && !slot_found
+        vertical[index][ind] = unicode
+        slot_found
+        break
+      end
+    end
+  end
+
+  def update_diagonal(input, unicode)
+    @diagonal = [[],[]]
+    check_sequences(input, unicode, 'forward')
+    check_sequences(input, unicode, 'backward')
+  end
+
+  def diagonal_points(input, dir)
+    return nil if input.nil?
+    if dir == 'forward'
+      [[input[0]+1,input[1]+1],[input[0]-1,input[1]-1]]
+    elsif dir == 'backward'
+      [[input[0]-1,input[1]+1], [input[0]+1,input[1]-1]]
+    end
+  end
+
+  def check_sequences(input, token, dir, queue = [], checked = [], dir2 = diagonal_points(input,dir))
+    return nil if input.nil?
+
+    2.times do |x|
+      slot_attr = dir2.shift
+      if slot_attr.all? { |attr| attr.between?(0,6) }
+        queue << slot_attr if vertical[slot_attr[0]][slot_attr[1]] == token
+      end
+    end
+
+    diagonal[dir == 'forward' ? 0 : 1] << token
+    checked << input
+    check_sequences((queue -= checked).shift, token, dir, queue, checked)
+  end
+end
